d5ab9acf1f68333cfc2d9ba5529a878960e57e0f
Fabian Hoszowski <fabix43@gmail.com>
2020-04-29 17:27:00 +0200

    Add new Activity: Welcome Screen Add new icon.

---
+++ app/src/main/java/com/name/accelerometr/WelcomeScreen.java
@@ -0,0 +1,33 @@
+package com.name.accelerometr;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+
+        button = findViewById(R.id.nextActivity);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
+    }
+
+    private void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+}
